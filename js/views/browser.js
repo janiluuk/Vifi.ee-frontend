e8a47e17292f95d0eaf634f37e13c417e73fde5f
Vifi server <root@backend.vifi.ee>
2015-07-05 22:39:12 +0200

    Added template for banners

--- js/views/browser.js
+++ js/views/browser.js
@@ -5,7 +5,7 @@ App.Views.BrowserPage = Backbone.View.extend({
         'change #search-form select': 'onSearchFieldChange',
         'change #search-form input[type="text"]': 'onSearchFieldChange',
         'change #search-form input[type="hidden"]': 'onSearchFieldChange',
-        'click #loadMore' : 'onLoadMore'
+        'click #loadMore': 'onLoadMore'
     },
     initialize: function(options) {
         this.options = options;
@@ -14,70 +14,73 @@ App.Views.BrowserPage = Backbone.View.extend({
         this.collection = options.collection;
         this.collection.options.genres.bind('all', this.setGenreDropDown, this);
         this.collection.fullCollection.bind('reset', this.renderResults, this);
-
         this.collection.querystate.bind('change', this.onChangeCollectionState, this);
         this.collection.querystate.bind('change:genres', this.onChangeGenre, this);
         this.collection.querystate.bind('change:durations', this.onChangeDuration, this);
         this.collection.querystate.bind('change:years', this.onChangeYear, this);
-        this.filterview = new App.Views.FilterView({filters: this.options.filters, sort: this.options.sort, state: this.collection.querystate});
+        this.filterview = new App.Views.FilterView({
+            filters: this.options.filters,
+            sort: this.options.sort,
+            state: this.collection.querystate
+        });
         this.filterview.bind('filter-bar:sort', this.onSort, this);
         this.filterview.bind('filter-bar:clear', this.onClear, this);
-        this.searchview = new App.Views.SearchView({model: this.collection.querystate});
-
-        this.searchview.render();
-        _.bindAll(this,'render', 'renderResults','initEvents', 'applyIsotope');
-
- 
-        this.initEvents();
-
-    },
-
-    initEvents: function() { 
-        
-    this.on("maximize", function() {  
-            
-            var height = this.originalHeight;
-
-            $("#search").animate({ "opacity": 0, "height": height, "min-height": height},100,false, function() { 
-
-                $("#front-page-slider").css({"min-height": height, "height":height}).show().animate({ "opacity": 1},300, "swing", function() { 
-                $(this).hide(); 
-                }.bind(this));
-                window.mySwiper.resizeFix();   
-             
-            });
-       
+        this.searchview = new App.Views.SearchView({
+            model: this.collection.querystate
         });
-
-
-        this.on("minimize", function() { 
-            this.originalHeight = $("#front-page-slider").height();
-
-            var height =  $(".feature-wrapper#search").css("min-height");
-
-            $("#front-page-slider").animate({"min-height":  height, "height": height, "opacity": 0},300, false,function() {  
-                      $("#search").css({"height" :  height, "min-height":  height});
-                      $(this).hide();
-                $("#search, #search-text").show().animate({ "opacity": 1},200);
-            });
-
-        }); 
+        this.searchview.render();
+        _.bindAll(this, 'render', 'renderResults', 'initEvents', 'applyIsotope');
+        this.initEvents();
     },
-
-
+    initEvents: function() {
+        this.on("maximize", function() {
+            var height = this.originalHeight;
+            $("#search").animate({
+                "opacity": 0,
+                "height": height,
+                "min-height": height
+            }, 200, false, function() {
+                $("#front-page-slider").css({
+                    "min-height": height,
+                    "height": height
+                }).animate({
+                    "opacity": 1
+                }, 300, "swing", function() {
+                    $("#front-page-slider").show();
+                    $(this).hide();
+                    window.mySwiper.resizeFix();
+                }.bind(this));
+                window.mySwiper.resizeFix();
+            });
+        });
+        this.on("minimize", function() {
+            this.originalHeight = $("#front-page-slider").height();
+            var height = $(".feature-wrapper#search").css("min-height");
+            $("#front-page-slider").animate({
+                "min-height": height,
+                "height": height,
+                "opacity": 0
+            }, 300, false, function() {
+                $("#search").css({
+                    "height": height,
+                    "min-height": height
+                });
+                $(this).hide();
+                $("#search, #search-text").show().animate({
+                    "opacity": 1
+                }, 200);
+            });
+        });
+    },
     render: function() {
         this.$el.html(ich.browserPageTemplate());
         this.filterview.render();
         this.applyIsotope();
         this.updateUIToState();
-
-        return this;  
+        return this;
     },
-
-
     applyIsotope: function() {
         /* Enable isotope on the results */
-
         this.$isotope = $("#content-body-list").isotope({
             layoutMode: 'fitRows',
             resizable: true,
@@ -85,12 +88,12 @@ App.Views.BrowserPage = Backbone.View.extend({
             transitionDuration: '0.4s',
             // disable scale transform transition when hiding
             hiddenStyle: {
-            opacity: 0,
-            'transform': 'translateY(100%)',
+                opacity: 0,
+                'transform': 'translateY(100%)',
             },
             visibleStyle: {
-            opacity: 1,
-            'transform': 'translateY(0%)',
+                opacity: 1,
+                'transform': 'translateY(0%)',
             },
             animationOptions: {
                 duration: 250,
@@ -98,19 +101,20 @@ App.Views.BrowserPage = Backbone.View.extend({
                 queue: true,
             }
         });
-
-        this.$isotope.isotope( 'on', 'layoutComplete', function() { setTimeout(function() { App.Utils.lazyload(); }, 120); } );
-
+        this.$isotope.isotope('on', 'layoutComplete', function() { 
+            setTimeout(function() {
+                App.Utils.lazyload();
+            }, 120);
+        });
         return true;
     },
-
     setGenreDropDown: function(action) {
         $('#id_genres').empty();
         if (this.collection.options.genres.length > 0) {
             if (this.collection.options.genres.length > 1) {
                 $('#id_genres').append(new Option('All Genres', ''));
             }
-            _.each(this.collection.options.genres.models, function(genre, key, list) {  
+            _.each(this.collection.options.genres.models, function(genre, key, list) {
                 $('#id_genres').append(new Option(genre.get("name"), genre.get("id")));
             });
             this.$('#id_genres option[value="' + this.collection.querystate.get('genres') + '"]').attr('selected', 'selected');
@@ -121,7 +125,6 @@ App.Views.BrowserPage = Backbone.View.extend({
     },
     onChangeDuration: function(model, duration) {
         //This is a state change event, not a dom event
-        
         if (this.options.redirect_on_duration_change && duration != this.collection.initial_search.duration) {
             this.redirectToBaseURL();
         }
@@ -135,180 +138,145 @@ App.Views.BrowserPage = Backbone.View.extend({
     onSort: function(field, desc) {
         this.onLoadingStart();
         this.collection.sortByAttribute(field, desc);
-
         return false;
     },
-    
-    onLoadingStart: function() { 
-
+    onLoadingStart: function() {
         $("#content-body-list").addClass("fadeDownList");
-        setTimeout(function() { $("#content-body-list").parent().addClass("loading"); },200);
+        setTimeout(function() {
+            $("#content-body-list").parent().addClass("loading");
+        }, 200);
         return false;
-
     },
     onLoadingEnd: function() { 
-        
         $("#content-body-list").removeClass("fadeDownList");
-        setTimeout(function() { $("#content-body-list").parent().removeClass("loading"); },350);
+        setTimeout(function() {
+            $("#content-body-list").parent().removeClass("loading");
+        }, 350);
         return false;
-
     },
     onClear: function(e) {
-
         this.collection.querystate.set(this.collection.querystate.defaults);
-        this.onChangeCollectionState(this.collection.querystate,true);
+        this.onChangeCollectionState(this.collection.querystate, true);
         return false;
-    },   
+    },
     onChangeGenre: function(model, genre) {
         // this function is a model state change, not the dom event: change
         // because of this we don't need the "event" arg.
         var parts = app.homepage.collection.querystate.get('genres');
-                if (undefined == parts || parts.length == 0) return false;
-        _.each(parts.split(";"), function(i) { $(".selection-wrapper [data-val="+i+"]")});
+        if (undefined == parts || parts.length == 0) return false;
+        _.each(parts.split(";"), function(i) {
+            $(".selection-wrapper [data-val=" + i + "]")
+        });
         if (this.options.redirect_on_genre_change && genre != this.collection.initial_search.genre) {
             this.redirectToBaseURL();
         }
     },
-
-
     onSearchFieldChange: function(event) {
-
         this.onLoadingStart();
         var value = $("#main-search-box").val();
-        var search_array = {            
+        var search_array = {
             q: value,
             genres: undefined,
             periods: undefined,
             durations: undefined
         };
-
         var search_dict = _.extend({}, search_array);
-
         $("#search-form select option[selected=selected]").each(function() {
-
             var fieldid = $(this).parent().attr("id");
             var fieldname = fieldid.replace("id_", "");
             var val = $(this).val();
             search_dict[fieldname] = search_dict[fieldname] == undefined ? val : search_dict[fieldname] += ";" + val;
         });
-        if (JSON.stringify(search_dict) != JSON.stringify(this.collection.querystate.attributes))
-        {
+        if (JSON.stringify(search_dict) != JSON.stringify(this.collection.querystate.attributes)) {
             this.collection.querystate.set(search_dict);
-
-        } else { 
-
+        } else {
             this.onLoadingEnd();
         }
     },
-
-    addOne : function ( item ) {
-        var view = new App.Views.FilmView({model:item});
+    addOne: function(item) {
+        var view = new App.Views.FilmView({
+            model: item
+        });
         var el = view.render().el;
         return el;
     },
-    addSet : function ( collection ) {
+    addSet: function(collection) {
         var container = document.createDocumentFragment();
         var _this = this;
         var items = [];
-        collection.each(function(film, id) { 
+        collection.each(function(film, id) {
             var el = _this.addOne(film);
             items.push(el);
             container.appendChild(el);
         });
-
-        this.$isotope.append(container).isotope('insert', items);        
+        this.$isotope.append(container).isotope('insert', items);
         return true;
-
     },
     onLoadMore: function(e) {
         e.preventDefault();
-
         $("#loadMore").addClass("loading");
-
-        setTimeout(function() { 
+        setTimeout(function() {
             if (this.addSet(this.collection.getNextPage())) {
-                $("#loadMore").removeClass("loading"); 
+                $("#loadMore").removeClass("loading");
             }
             if (!this.collection.hasNextPage()) {
                 $("#loadMore").hide();
             }
-
-        }.bind(this),200);
-    
+        }.bind(this), 200);
         return false;
     },
-
     renderResults: function(force) {
-        
-
         if (typeof(force) == "undefined" && (typeof(this.lastattributes) != "undefined") || this.lastattributes == JSON.stringify(this.collection.querystate.changedAttributes())) {
-            
             return false;
         }
-
         if (!this.rendering) {
-
             this.rendering = true;
             $("#content-body-list").addClass("fadeDownList").empty().parent().addClass("loading");
             this.$isotope.isotope("reloadItems");
-
             this.collection.getFirstPage();
-
             /* Empty result set */
             if (this.collection.length == 0) {
-
-                $("#content-body-list").append(ich.emptyListTemplate({text: tr("No results")})).parent().removeClass("loading");
-                
-            }   
-            
+                $("#content-body-list").append(ich.emptyListTemplate({
+                    text: tr("No results")
+                })).parent().removeClass("loading");
+            }
             this.addSet(this.collection);
-
             if (!this.collection.hasNextPage()) {
                 $("#loadMore").hide();
-            } else { 
+            } else {
                 $("#loadMore").show();
             }
             this.lastattributes = JSON.stringify(this.collection.querystate.changedAttributes());
             this.rendering = false;
         }
-
         this.onLoadingEnd();
-
-
-        return false;        
+        return false;
     },
     updateUIToState: function() {
         var state = this.collection.querystate;
         // main search text box
         var query = state.get('q');
-
         $('#main-search-box').val(query).trigger("keyup");
-
         this.filterview.updateUI();
-
     },
-
-    onChangeCollectionState: function(state,silent) {
-
+    onChangeCollectionState: function(state, silent) {
         var trigger = silent === true ? false : true;
         this.updateUIToState();
-
-        _.extend(this.collection.queryParams, this.collection.querystate.attributes);        
+        _.extend(this.collection.queryParams, this.collection.querystate.attributes);
         //Update the url of the browser using the router navigate method
-        app.router.navigate('search' + '?' + app.homepage.collection.querystate.getHash(), {trigger: trigger});
+        app.router.navigate('search' + '?' + app.homepage.collection.querystate.getHash(), {
+            trigger: trigger
+        });
     },
-
     //Set the search state from the url
     setSearchStateFromHash: function(searchStateHash) {
         //setFromHash will trigger a change event, which then
         //loads the records and reloads the table
         this.collection.querystate.setFromHash(searchStateHash);
-
     },
     clearSearch: function() {
-        app.collection.querystate.set("q","");
+        app.collection.querystate.set("q", "");
         this.onChangeCollectionState(app.collection.querystate);
         return false;
     }
 });
-
